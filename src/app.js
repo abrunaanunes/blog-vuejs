b6d8040e4a7b96bcccde7903d27ece644738dc5b
Bruna Nunes <brunacris.2404@gmail.com>
2024-06-05 21:51:08 -0300

    Adicionando lib FontAwesome

--- src/app.js
+++ src/app.js
@@ -2,7 +2,10 @@ import { createApp } from 'vue'
 import './app.css'
 import App from './App.vue'
 import router from './router.js'
+import FontAwesomeIcon from "./components/FontAwesomeIcon.vue"
+
 
 createApp(App)
   .use(router)
+  .component("fa-icon", FontAwesomeIcon)
   .mount('#app')
---
+++ src/components/FontAwesomeIcon.vue
@@ -0,0 +1,28 @@
+<script>
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+    faPen,
+    faTrash,
+    faEye,
+    faUser,
+    faSun,
+    faMoon,
+    faFileExcel,
+    faFilePdf,
+    faMap,
+    faEnvelope,
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(faPen)
+library.add(faTrash)
+library.add(faEye)
+library.add(faUser)
+library.add(faSun)
+library.add(faMoon)
+library.add(faFileExcel)
+library.add(faFilePdf)
+library.add(faMap)
+library.add(faEnvelope)
+export default FontAwesomeIcon;
+</script>
