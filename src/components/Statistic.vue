dff8304fe45eece85e223a6ec31939b4e2a26e35
Bruna Nunes <brunacris.2404@gmail.com>
2024-06-05 21:52:45 -0300

    Adicionando componente estatísticas e MSG erro

--- src/components/Statistic.vue
+++ src/components/Statistic.vue
@@ -1,26 +1,26 @@
 <script setup>
-    import { posts } from '../../database/database.js'
+    import { posts, categories, authors } from '../../database/database.js'
 </script>
 
 <template>
     <div>
-        <h3><span class="bg-[034c46] text-white"> Novidades </span> de Dia</h3>
+        <h3><span class="bg-[#034c46] text-white"> Estatísticas </span> do Dia</h3>
         <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
-          <div class="bg-[#f3f8f7] rounded-md p-8 flex flex-col justify-center items-center gap-2">
+          <div class="bg-[#f3f8f7] dark:bg-[#0D1117] rounded-md p-8 flex flex-col justify-center items-center gap-2">
             <p class="text-4xl font-medium text-[034c46]">{{ posts?.length ?? 0 }}</p>
             <p>Publicações</p>
           </div>
-          <div class="bg-[#f3f8f7] rounded-md p-8 flex flex-col justify-center items-center gap-2">
-            <p class="text-4xl font-medium text-[034c46]">14</p>
-            <p>New Posts</p>
+          <div class="bg-[#f3f8f7] dark:bg-[#0D1117] rounded-md p-8 flex flex-col justify-center items-center gap-2">
+            <p class="text-4xl font-medium text-[034c46]">{{ authors?.length ?? 0 }}</p>
+            <p>Autores</p>
           </div>
-          <div class="bg-[#f3f8f7] rounded-md p-8 flex flex-col justify-center items-center gap-2">
-            <p class="text-4xl font-medium text-[034c46]">14</p>
-            <p>New Posts</p>
+          <div class="bg-[#f3f8f7] dark:bg-[#0D1117] rounded-md p-8 flex flex-col justify-center items-center gap-2">
+            <p class="text-4xl font-medium text-[034c46]">{{ categories?.length ?? 0 }}</p>
+            <p>Categorias</p>
           </div>
-          <div class="bg-[#f3f8f7] rounded-md p-8 flex flex-col justify-center items-center gap-2">
-            <p class="text-4xl font-medium text-[034c46]">14</p>
-            <p>New Posts</p>
+          <div class="bg-[#f3f8f7] dark:bg-[#0D1117] rounded-md p-8 flex flex-col justify-center items-center gap-2">
+            <p class="text-4xl font-medium text-[034c46]">11</p>
+            <p>Leitores</p>
           </div>
         </div>
       </div>
---
+++ src/components/ValidationMessage.vue
@@ -0,0 +1,7 @@
+<script setup>
+    defineProps(['show', 'text'])
+</script>
+
+<template>
+    <span v-if="show" class="w-fit rounded-md p-1 bg-red-100 dark:bg-[#161B22] text-xs text-red-400 text-left block mt-2">{{text}}<br></span>
+</template>
