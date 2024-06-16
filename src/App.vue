8dec2e4b298fccd28d4faaff344d9d82a36bf4c2
Bruna Nunes <brunacris.2404@gmail.com>
2024-06-15 22:28:32 -0300

    Excluindo código não utilizado

--- src/App.vue
+++ src/App.vue
@@ -32,11 +32,6 @@
   <header class="w-full px-6 lg:px-36 py-6 flex flex-row justify-between border-b border-gray-100 shadow-sm dark:border-none dark:bg-[#161B22] dark:text-gray-400">
     <RouterLink to="/"><h1 class="font-medium"><span class="bg-[#034c46] text-white">Blog</span>book</h1></RouterLink>
 
-    <!-- <form class="hidden md:flex flex-items justify-center items-center" @submit.prevent="submit">
-      <input class="border border-gray-200 p-2 dakr:bg-[#0D1117]" type="text" placeholder="Pesquisar por artigos..." v-model="form.query">
-      <button class="bg-[#034c46] text-white h-full p-2 uppercase font-medium" type="submit">Buscar</button>
-    </form> -->
-
     <form class="max-w-md mx-auto" @submit.prevent="submit">
       <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Buscar</label>
       <div class="relative">
@@ -45,8 +40,8 @@
                   <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
               </svg>
           </div>
-          <input type="search" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-emerald-500 focus:border-emerald-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-emerald-500 dark:focus:border-emerald-500" placeholder="Pesquisar..." required />
-          <button type="submit" class="text-white absolute end-2.5 bottom-2.5 bg-emerald-700 hover:bg-emerald-800 focus:ring-4 focus:outline-none focus:ring-emerald-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-emerald-600 dark:hover:bg-emerald-700 dark:focus:ring-emerald-800">Buscar</button>
+          <input type="search" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-md bg-gray-50 focus:ring-emerald-500 focus:border-emerald-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-emerald-500 dark:focus:border-emerald-500" placeholder="Pesquisar..." required />
+          <button type="submit" class="text-white absolute end-2.5 bottom-2.5 bg-emerald-700 hover:bg-emerald-800 focus:ring-4 focus:outline-none focus:ring-emerald-300 font-medium rounded-md text-sm px-4 py-2 dark:bg-emerald-600 dark:hover:bg-emerald-700 dark:focus:ring-emerald-800">Buscar</button>
       </div>
     </form>
 
